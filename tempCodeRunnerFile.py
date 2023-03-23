12d50c8d73e835931ff7192a53d6c6a7d4611aba
Niyazi Ural <niyazi.ural@outlook.com>
2023-03-23 21:20:55 +0300

    removed tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-endpoint = "me/player/recently-played"
-# query = "?limit=1"
-# header = {
-#     "Authorization": "Bearer " + access_token,
-#     "Content-Type": "application/json"
-# }
-
-# response = requests.get(api_url + endpoint + query, headers=header)
-# print(json.dumps(response.json(), indent=4))
